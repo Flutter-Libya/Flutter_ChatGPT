489c14841c1c6a19ee894b5eb08d36a708c16e50
Flutter-Libya <131480121+Flutter-Libya@users.noreply.github.com>
2023-11-23 00:05:37 +0200

    Update injection_container.dart

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,10 +1,8 @@
-
-
-
+import 'package:http/http.dart' as http;
 import 'package:flutter_chatgpt/features/image_generation/image_generation_injection_container.dart';
 import 'package:flutter_chatgpt/features/text_completion/text_completion_injection_container.dart';
 import 'package:get_it/get_it.dart';
-import 'package:http/http.dart' as http;
+
 
 final sl = GetIt.instance;
 
